4700b19cacc064b325c8404a101e541f69176fc6
GABRIEL DINIZ FREIRE MENDES <202307164641@alunos.ibmec.edu.br>
2023-10-30 15:15:32 -0300

    codigo atletas

--- desweb-js/codigo.js
+++ desweb-js/codigo.js
@@ -1,12 +1,38 @@
+const container_filtro = document.createElement('div');
+const texto_pesquisa = document.createElement('input');
+const btn_pesquisa = document.createElement('button');
+
+container_filtro.style.textAlign = 'center';
+
+btn_pesquisa.innerHTML = 'Pesquisar';
+btn_pesquisa.onclick = () => {
+    atletas_container.innerHTML = '';
+    criar_atletas(filtrar_atletas(texto_pesquisa.value));
+};
+
+container_filtro.appendChild(texto_pesquisa);
+container_filtro.appendChild(btn_pesquisa);
+
+document.body.appendChild(container_filtro);
+
+const atletas_container =  document.createElement('div');
+document.body.appendChild(atletas_container);
+
+
 const preenche = (atleta) => {
-    
-    const container = document.createElement('div')
-    const titulo = document.createElement('h3')
-    const imagem = document.createElement('img')
-    const descricao = document.createElement('p')
+    const container = document.createElement('div');
+    const titulo = document.createElement('h3');
+    const imagem = document.createElement('img');
+    const descricao = document.createElement('p');
+
+    container.style.width = '15em';
+    container.style.backgroundColor = 'gray';
+    container.style.textAlign = 'center';
+    container.style.margin = 'auto';
 
     titulo.innerText = atleta.nome;
     imagem.src = atleta.imagem;
+    imagem.alt = `Imagem de ${atleta.nome}`;
     descricao.innerHTML = atleta.descricao;
 
     container.appendChild(titulo);
@@ -16,15 +42,27 @@ const preenche = (atleta) => {
     document.body.appendChild(container);
 }
 
-/*
-let index = 0;
-while (index < atletas.length) {
-    preenche(atletas[index]);
-    index++;
-}*/
-
-
-for(let index = atletas.length - 1; index >= 0; index--){
-    preenche(atletas[index]);
-    console.log(index, 'loop for');
+const criar_atletas = (entrada) => {
+    for (let indice = 0; indice < entrada.length; indice++){
+        preenche(entrada[indice]);
+    }
 }
+
+
+const filtrar_atletas = (texto) => {
+    const lista_filtrada = atletas.filter(
+        (e) => {
+            const nome = e.nome;
+            const nome_minusculas = nome.toLocaleLowerCase();
+            const texto_minusculas = texto.toLocaleLowerCase();
+            const resultado = nome_minusculas.includes(texto_minusculas);
+            return resultado;
+        }
+    )
+
+    return lista_filtrada;
+}
+
+
+criar_atletas(filtrar_atletas('y'));
+
